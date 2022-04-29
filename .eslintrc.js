c6c495298dfd108f79b0a82ef9615ddcb075f0ca
hungnp194 <61738572+hungnp194@users.noreply.github.com>
2022-04-29 11:46:01 +0700

    Config deploy (#11)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,21 @@ module.exports = {
   root: true,
 
   env: {
-    node: true,
-  
+    node: true
   },
 
   extends: [
-    "plugin:vue/essential",
-    "plugin:prettier/recommended",
-    "eslint:recommended",
-    "@vue/typescript",
-  ],
-
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-   
-  },
-
-  rules: {
-    'vue/multi-word-component-names': 0
-  },
-
-  'extends': [
     'plugin:vue/essential',
     'plugin:prettier/recommended',
     'eslint:recommended',
     '@vue/typescript'
-  ]
+  ],
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+
+  rules: {
+    'vue/multi-word-component-names': 0
+  }
 };
