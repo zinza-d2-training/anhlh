f3f8d23ac3c532dcac2d714498b40331de5c80b7
azinza <anh.lh@zinza.com.vn>
2022-06-21 09:18:58 +0700

    Write type return for Generic type of axios

--- src/store/user/getters.ts
+++ src/store/user/getters.ts
@@ -3,7 +3,6 @@ import { UserState } from './type';
 
 export const getters: GetterTree<UserState, any> = {
   getEmail(state): string {
-    console.log(state.email);
     return state.email;
   },
   getToken(state): string | undefined {
